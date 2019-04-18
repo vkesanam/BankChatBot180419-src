666bdf0145b98f8bcea6930650113ebbd8d4a5b7
VenkataSambasivaRao <sambakesanam@gmail.com>
2019-04-18 15:08:30 +0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bankchatbot180419-Web-Deploy.pubxml -p:Password=TQZ8AlntaNl3HLrtoup7EgBpusjLzJNpePWC4MDE8ef4lwf8T5AuljpvMGEg
+
